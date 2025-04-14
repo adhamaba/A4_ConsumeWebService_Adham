d275c43f47da3f898a2324738a4a1810f4d92925
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 00:22:20 -0400

    Add BookListComponent to display all books with actions

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService, Book } from '../../services/book.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
-  templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  templateUrl: './book-list.component.html'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.bookService.getAll().subscribe(data => {
+      this.books = data;
+    });
+  }
+
+  goToEdit(id: number) {
+    this.router.navigate(['/edit-book', id]);
+  }
+
+  goToDelete(id: number) {
+    this.router.navigate(['/delete-book', id]);
+  }
+
+  goToDetails(id: number) {
+    this.router.navigate(['/book-details', id]);
+  }
 }
