72e5e42a98f79d42fee32408e3ac7d6c4a38c820
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 00:15:30 -0400

    Add routing configuration for book components and fallback route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { FindBookComponent } from './components/find-book/find-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'add-book', component: AddBookComponent },
+  { path: 'edit-book/:id', component: EditBookComponent },
+  { path: 'delete-book/:id', component: DeleteBookComponent },
+  { path: 'find-book', component: FindBookComponent },
+  { path: 'book-details/:id', component: BookDetailsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
