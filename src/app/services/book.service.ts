c14bfff1cbb9af20813aa04a2d3fd7137b0bcf2f
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 00:22:32 -0400

    Implement BookService with full CRUD and search methods

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Book {
+  id?: number;
+  title: string;
+  authorName: string;
+  price: number;
+  quantity: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  create(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  update(id: number, book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  findByTitle(title: string): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiUrl}/search?title=${title}`);
+  }
 }
