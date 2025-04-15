ca28feafbbcab76e76af20ac165cebe10124f482
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 21:00:25 -0400

    Add BookDetailsComponent to display full book info by ID

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService, Book } from '../../services/book.service';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
-  templateUrl: './book-details.component.html',
-  styleUrl: './book-details.component.css'
+  templateUrl: './book-details.component.html'
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  book: Book | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getById(id).subscribe(data => {
+      this.book = data;
+    });
+  }
+
+  back(): void {
+    this.router.navigate(['/books']);
+  }
 }
