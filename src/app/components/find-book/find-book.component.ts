9d5d67c47232aca976018c2920ff00c19fbf0c21
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 20:59:24 -0400

    Add FindBookComponent with search functionality by title

--- src/app/components/find-book/find-book.component.ts
+++ src/app/components/find-book/find-book.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { BookService, Book } from '../../services/book.service';
 
 @Component({
   selector: 'app-find-book',
-  imports: [],
-  templateUrl: './find-book.component.html',
-  styleUrl: './find-book.component.css'
+  templateUrl: './find-book.component.html'
 })
 export class FindBookComponent {
+  searchTitle = '';
+  results: Book[] = [];
+  message = '';
 
+  constructor(private bookService: BookService) {}
+
+  search(): void {
+    if (this.searchTitle.trim()) {
+      this.bookService.findByTitle(this.searchTitle.trim()).subscribe(data => {
+        this.results = data;
+        this.message = data.length > 0 ? '' : 'No books found.';
+      });
+    } else {
+      this.results = [];
+      this.message = 'Please enter a title to search.';
+    }
+  }
 }
