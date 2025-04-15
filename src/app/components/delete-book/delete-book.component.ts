773f37d9c1820f2a46a96e9255faafc12fd1507a
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 20:57:45 -0400

    Add DeleteBookComponent with confirmation UI and delete logic

--- src/app/components/delete-book/delete-book.component.ts
+++ src/app/components/delete-book/delete-book.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService, Book } from '../../services/book.service';
 
 @Component({
   selector: 'app-delete-book',
-  imports: [],
-  templateUrl: './delete-book.component.html',
-  styleUrl: './delete-book.component.css'
+  templateUrl: './delete-book.component.html'
 })
-export class DeleteBookComponent {
+export class DeleteBookComponent implements OnInit {
+  book: Book | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getById(id).subscribe(data => {
+      this.book = data;
+    });
+  }
+
+  deleteBook(): void {
+    if (this.book?.id) {
+      this.bookService.delete(this.book.id).subscribe(() => {
+        this.router.navigate(['/books']);
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/books']);
+  }
 }
