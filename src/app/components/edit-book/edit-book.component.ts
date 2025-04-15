4c52e72f9ae50166505278c03a0568ee547c6a5e
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 20:54:10 -0400

    Add EditBookComponent to edit book details by ID

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService, Book } from '../../services/book.service';
 
 @Component({
   selector: 'app-edit-book',
-  imports: [],
-  templateUrl: './edit-book.component.html',
-  styleUrl: './edit-book.component.css'
+  templateUrl: './edit-book.component.html'
 })
-export class EditBookComponent {
+export class EditBookComponent implements OnInit {
+  book: Book = {
+    title: '',
+    authorName: '',
+    price: 0,
+    quantity: 0
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getById(id).subscribe(data => {
+      this.book = data;
+    });
+  }
+
+  updateBook(): void {
+    if (this.book.id) {
+      this.bookService.update(this.book.id, this.book).subscribe(() => {
+        this.router.navigate(['/books']);
+      });
+    }
+  }
 }
