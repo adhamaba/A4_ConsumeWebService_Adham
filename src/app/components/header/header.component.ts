07d577c6ed7a2e912bc8a9e51317f7004dd32342
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-14 21:05:35 -0400

    Add Header, Footer, and PageNotFound components with layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
-  templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
+  templateUrl: './page-not-found.component.html'
 })
-export class PageNotFoundComponent {
-
-}
+export class PageNotFoundComponent {}
